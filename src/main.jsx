f499a495c9836970692a61ea08105e5e72849375
ServiEnergy <info.digitalconect@gmail.com>
2026-02-24 20:03:12 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ContaAuto from './ContaAuto.jsx'
+import ContaAuto from '../ContaAuto.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
