1db93bf2b6cc521daa4120c3eb9124fb99712e63
ServiEnergy <info.digitalconect@gmail.com>
2026-02-24 19:41:03 +0100

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ContaAuto from './ContaAuto.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ContaAuto />
+  </React.StrictMode>
+)
